38a17f5c74acc63e134726d5e107aa7c1873a3b2
Ebrahim <ebrahimgamal005@gmail.com>
2025-08-01 17:10:44 +0300

    activity_finance.xml  and FinanceActivity.kt

--- app/src/main/java/com/example/personalfinancetrackerapp/FinanceActivity.kt
+++ app/src/main/java/com/example/personalfinancetrackerapp/FinanceActivity.kt
@@ -1,4 +1,43 @@
 package com.example.personalfinancetrackerapp
 
-class FinanceActivity {
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class FinanceActivity : AppCompatActivity() {
+    val TAG = "Lifecycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_finance)
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
 }
+
