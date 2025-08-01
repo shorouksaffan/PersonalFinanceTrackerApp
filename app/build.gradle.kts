73788f232923c57f0a31ddeb81eab64938759266
smsm <smsmelbidawy162@gmail.com>
2025-08-01 19:58:02 +0300

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
+import org.gradle.api.JavaVersion
+
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
@@ -36,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/com/example/personalfinancetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/personalfinancetrackerapp/MainActivity.kt
@@ -1,4 +1,64 @@
 package com.example.personalfinancetrackerapp
 
-class MainActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val TAG = "Lifecycle"
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val trackExpenseButton = findViewById<Button>(R.id.btnTrack)
+        val shareButton = findViewById<Button>(R.id.btnShare)
+
+        trackExpenseButton.setOnClickListener {
+            val intent = Intent(this, FinanceActivity::class.java)
+            startActivity(intent)
+            val TAG = "Lifecycle"
+            Log.d(TAG, "navigate success")
+        }
+        shareButton.setOnClickListener {
+
+            val TAG = "Lifecycle"
+
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, "I just added a new expense: Lunch – $10")
+            }
+            Log.d(TAG, "share button clicked")
+            startActivity(Intent.createChooser(shareIntent, "Share expense via"))
+        }
+    }
+    val TAG = "Lifecycle"
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
 }
