0da304557876ef8e48beffd5b8d1b2da32083ed7
m7mdfox <fcb.mohamed2005@gmail.com>
2025-08-03 02:49:17 +0300

    changed project to view binding finance logic done

--- app/src/main/java/com/example/personalfinancetrackerapp/FinanceActivity.kt
+++ app/src/main/java/com/example/personalfinancetrackerapp/FinanceActivity.kt
@@ -1,18 +1,73 @@
 package com.example.personalfinancetrackerapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.set
 import com.example.personalfinancetrackerapp.R
+import com.example.personalfinancetrackerapp.databinding.ActivityFinanceBinding
+import com.example.personalfinancetrackerapp.databinding.ActivityMainBinding
 
 class FinanceActivity : AppCompatActivity() {
+
+    private var _binding: ActivityFinanceBinding? = null
+    private val binding get() = _binding!!
+
     val TAG = "Lifecycle"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_finance)
+        _binding = ActivityFinanceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSave.setOnClickListener {
+            if (binding.editExpenseName.text.isNullOrBlank()||binding.editExpenseName.text.isEmpty()){
+                Toast.makeText(this,
+                    getString(R.string.expenses_name_can_not_be_empty), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (binding.editExpenseName.text.toString().lowercase().filter { it >= 'a' && it <= 'z' }.length != binding.editExpenseName.text.toString().length){
+                Toast.makeText(this,
+                    getString(R.string.expenses_name_must_contain_letters_only), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (binding.editAmount.text.isNullOrBlank()||binding.editAmount.text.isEmpty()) {
+                Toast.makeText(this,
+                    getString(R.string.the_amount_can_not_be_empty), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (binding.editAmount.text.toString().toDoubleOrNull()==null){
+                Toast.makeText(this,
+                    getString(R.string.the_amount_must_be_integers_only), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (binding.editCategory.text.isNullOrBlank()||binding.editCategory.text.isEmpty()){
+                Toast.makeText(this,
+                    getString(R.string.category_name_can_not_be_empty), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (binding.editCategory.text.toString().lowercase().filter { it >= 'a' && it <= 'z' }.length != binding.editCategory.text.toString().length){
+                Toast.makeText(this,
+                    getString(R.string.category_name_must_contain_letters_only), Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val name = binding.editExpenseName.text.toString()
+            val amount = binding.editAmount.text.toString().toDoubleOrNull()!!
+            val category = binding.categoryTxt.text.toString()
+            Toast.makeText(this, getString(R.string.expenses_added_successfully), Toast.LENGTH_SHORT).show()
+            binding.editAmount.text.clear()
+            binding.editCategory.text.clear()
+            binding.editExpenseName.text.clear()
+            MainActivity.expenses.add(Expense(name = name, amount = amount, category = category))
+            MainActivity.adapter.notifyItemInserted(MainActivity.expenses.size-1)
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
 
         Log.d(TAG, "onCreate called")
     }
